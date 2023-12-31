6686f4dcc0bf2a1bef132c35dc50eaa51ae7a85f
Another <89122911+hexvel@users.noreply.github.com>
2023-12-31 19:38:30 +0300

    Update query.py

--- utils/query.py
+++ utils/query.py
@@ -6,7 +6,7 @@ class Request:
     def __init__(self):
         self.url = "https://api.pxolly.com/m/database.get"
         self.database_id = "iris"
-        self.access_token = "v8x.A.bZ6IajKfGa4Q.znJU27e8aFn2dyKuUinhijUP4m1vV_5dy2qmcxYdl9ZS8xiL48DnFN-9cA9PLQMUTvNTbbV.Tov8c"
+        self.access_token = "ага"
 
     async def get_iris_base(self, user_ids: int | list, allow_fakes: Literal["false", "true"]):
 
